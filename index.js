941e890728aaa1e881c00d64f686ff6177e73605
Tanvir Ahmed <parthotanvir1234@gmail.com>
2022-11-09 06:53:16 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const categories = require("./data/categories.json");
+app.get("/", (req, res) => {
+  res.send("programming API running");
+});
+
+app.get("/course-categories", (req, res) => {
+  res.send(categories);
+});
+app.listen(port, () => {
+  console.log("learn programming server running on port", port);
+});
